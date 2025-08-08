ed5f9c7f022be1869a2cab9292e4e0b61c24afa5
Zuhaitz <zuhaitz.zechhub@gmail.com>
2025-08-08 16:37:47 +0100

    Update update_version.sh (now we handle both BSD sed and GNU sed)

--- update_version.sh
+++ update_version.sh
@@ -17,19 +17,19 @@ echo "Detected old version: $OLD_VERSION"
 
 # Update the Makefile
 # The new Makefile will read the version directly from VERSION_FILE and CODENAME_FILE
-sed -i "s/VERSION := .*/VERSION := \"\$(shell cat .version\/VERSION_FILE) - (\$(shell cat .version\/CODENAME_FILE))\"/g" Makefile
+sed -i '' "s~VERSION := .*~VERSION := \"\$(shell cat .version/VERSION_FILE) - (\$(shell cat .version/CODENAME_FILE))\"~g" Makefile
 
 # Update the README.md
-sed -i "s/v0\.1\.0-beta/${NEW_VERSION_SIMPLE}/g" README.md
-sed -i "s/v0\.1\.0-beta - (Betelgeuse)/${NEW_VERSION_FULL}/g" README.md
+sed -i '' "s~${OLD_VERSION}~${NEW_VERSION_SIMPLE}~g" README.md
+sed -i '' "s~v0.1.0-beta - (Betelgeuse)~${NEW_VERSION_FULL}~g" README.md
 
 # Update the man page (fat.1)
-sed -i "s/v0\.1\.0-beta/${NEW_VERSION_SIMPLE}/g" man/fat.1
+sed -i '' "s~v0.1.0-beta~${NEW_VERSION_SIMPLE}~g" man/fat.1
 
 # Update the snapcraft.yaml file
 # This one requires special handling as it uses a different version format ('0.1.0')
 SNAP_OLD_VERSION=$(grep 'version:' snap/snapcraft.yaml | awk '{print $NF}' | tr -d "'")
 SNAP_NEW_VERSION=$(echo "$NEW_VERSION_SIMPLE" | sed 's/v//g' | sed 's/-beta//g')
-sed -i "s/version: .*/version: '${SNAP_NEW_VERSION}'/g" snap/snapcraft.yaml
+sed -i '' "s/version: .*/version: '${SNAP_NEW_VERSION}'/g" snap/snapcraft.yaml
 
 echo "Version update complete!"
