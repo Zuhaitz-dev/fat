00eb87b4e75feb34705c017dc64a99732dacee4e
Zuhaitz <zuhaitz.zechhub@gmail.com>
2025-08-08 16:40:56 +0100

    Trying to improve the script for each platform

--- update_version.sh
+++ update_version.sh
@@ -3,6 +3,15 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Detect the operating system and set the correct sed syntax for in-place editing
+if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS (BSD sed) requires a backup file extension, or an empty string for no backup
+    SED_INPLACE='-i ""'
+else
+    # Linux (GNU sed) does not require an extension, but can accept an empty string
+    SED_INPLACE='-i'
+fi
+
 # Get the new version and codename from their respective files
 NEW_VERSION_SIMPLE=$(cat .version/VERSION_FILE)
 NEW_CODENAME=$(cat .version/CODENAME_FILE)
@@ -17,19 +26,19 @@ echo "Detected old version: $OLD_VERSION"
 
 # Update the Makefile
 # The new Makefile will read the version directly from VERSION_FILE and CODENAME_FILE
-sed -i '' "s~VERSION := .*~VERSION := \"\$(shell cat .version/VERSION_FILE) - (\$(shell cat .version/CODENAME_FILE))\"~g" Makefile
+sed $SED_INPLACE "s~VERSION := .*~VERSION := \"\$(shell cat .version/VERSION_FILE) - (\$(shell cat .version/CODENAME_FILE))\"~g" Makefile
 
 # Update the README.md
-sed -i '' "s~${OLD_VERSION}~${NEW_VERSION_SIMPLE}~g" README.md
-sed -i '' "s~v0.1.0-beta - (Betelgeuse)~${NEW_VERSION_FULL}~g" README.md
+sed $SED_INPLACE "s~v0\.1\.0-beta~${NEW_VERSION_SIMPLE}~g" README.md
+sed $SED_INPLACE "s~v0\.1\.0-beta - (Betelgeuse)~${NEW_VERSION_FULL}~g" README.md
 
 # Update the man page (fat.1)
-sed -i '' "s~v0.1.0-beta~${NEW_VERSION_SIMPLE}~g" man/fat.1
+sed $SED_INPLACE "s~v0\.1\.0-beta~${NEW_VERSION_SIMPLE}~g" man/fat.1
 
 # Update the snapcraft.yaml file
 # This one requires special handling as it uses a different version format ('0.1.0')
 SNAP_OLD_VERSION=$(grep 'version:' snap/snapcraft.yaml | awk '{print $NF}' | tr -d "'")
 SNAP_NEW_VERSION=$(echo "$NEW_VERSION_SIMPLE" | sed 's/v//g' | sed 's/-beta//g')
-sed -i '' "s/version: .*/version: '${SNAP_NEW_VERSION}'/g" snap/snapcraft.yaml
+sed $SED_INPLACE "s/version: .*/version: '${SNAP_NEW_VERSION}'/g" snap/snapcraft.yaml
 
 echo "Version update complete!"
